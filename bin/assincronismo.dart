20c673380d2d2c230b25794123448d7805f013a0
IanS04 <107213634+IanS04@users.noreply.github.com>
2026-09-01 23:15:45 -0300

    Refactor requestData to use async function

--- bin/assincronismo.dart
+++ bin/assincronismo.dart
@@ -1,8 +1,10 @@
 import 'package:http/http.dart';
+import 'dart:convert';
 
 void main () {
-  print("Olá mundo!");
-  requestData();
+  //print("Olá mundo!");
+  //requestData();
+  RequestDataAsync();
 }
 
 void requestData() {
@@ -13,7 +15,22 @@ void requestData() {
     (Response response) {
     print(response);
     print(response.body);
-    
+      json.decode(response.body);
+      List<dynamic> listAccounts = json.decode(response.body);
+      Map<String, dynamic> mapCarla = listAccounts.firstWhere(
+        (element) => element["name"] == "Carla",
+      );
+      print(mapCarla["balance"]);
     },
   );
+
+  print("final da função");
+}
+
+Future<void> RequestDataAsync() async {
+  String url = 
+      "https://gist.githubusercontent.com/IanS04/fa71625296d41b610050fd507d760187/raw/b58ada841ad0e2061a29714673a332b18fd75bb2/accounts.json";
+  Response response = await get(Uri.parse(url));
+  print(json.decode(response.body)[0]);
+  print("ultima coisa a acontecer de verdade");
 }
