e116a421928f641e22fe350a8162d646cb59ff5d
IanS04 <107213634+IanS04@users.noreply.github.com>
2026-09-07 13:19:54 -0300

    Refactor AccountScreen chatbot interactions
    Updated chatbot assistant's name and improved user prompts.

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -1,5 +1,7 @@
 import 'package:assincronismo/models/account.dart';
+
 import 'package:assincronismo/services/account_service.dart';
+
 import 'dart:io';
 
 class AccountScreen {
@@ -14,56 +16,63 @@ class AccountScreen {
   }
 
   void runChatBot() async {
-    print("Bom dia!! Eu sou o Andre, assistente do Banco misterio");
-    print("Que bom ter você aqui com a gente.\n");
+    print("Bom dia! Eu sou o Lewis, assistente do Banco d'Ouro!");
+    print("Que bom te ter aqui com a gente.\n");
 
     bool isRunning = true;
-    while (isRunning){
-      print("Como posso te ajudar? (digite o número desejado)");
-      print("1 - Ver todas suas contas.");
-      print("2 - Adicionar nova conta.");
-      print("3 - sair\n");
+    while (isRunning) {
+      print("Como eu posso te ajudar? (digite o número desejado)");
+      print("1 - 👀 Ver todas sua contas.");
+      print("2 - ➕ Adicionar nova conta.");
+      print("3 - Sair\n");
 
       String? input = stdin.readLineSync();
 
-      if (input != null){
-        switch (input){
+      if (input != null) {
+        switch (input) {
           case "1":
-          {
-            await _getAllAccounts();
-            break;
-          }
+            {
+              await _getAllAccounts();
+              break;
+            }
           case "2":
-          {
-            _addExampleAccount();
-            break;
-          }
+            {
+              await _addExampleAccount();
+              break;
+            }
           case "3":
-          {
-            isRunning = false;
-            print("Te vejo na proxima");
-            break;
-          }
+            {
+              isRunning = false;
+              print("Te vejo na próxima. 👋");
+              break;
+            }
           default:
-            print("Não entendi, tente novamente");
+            {
+              print("Não entendi. Tente novamente.");
+            }
         }
       }
     }
   }
 
-  Future<void> _getAllAccounts() async {
-    List<Account> listAccounts = await _accountService.getAll();
-    print(listAccounts);
+  _getAllAccounts() async {
+    try {
+      List<Account> listAccounts = await _accountService.getAll();
+      print(listAccounts);
+    } on Exception {
+      print("Não consegui recuperar os dados da conta.");
+      print("Tente novamente mais tarde.");
+    }
   }
 
-  void _addExampleAccount() async {
+  _addExampleAccount() async {
     Account example = Account(
-      id: "ID555", 
-      name: "Haley", 
-      lastName: "Raven", 
-      balance: 9000,
-      );
+      id: "ID555",
+      name: "Haley",
+      lastName: "Chirívia",
+      balance: 8001,
+    );
 
-      await _accountService.addAccount(example);
+    await _accountService.addAccount(example);
   }
 }
